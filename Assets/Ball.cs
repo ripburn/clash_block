9ae8e135a240aebf78d8425e4a5770afa51ff356
PC141237\ic141237 <yuya.m.0712@gmail.com>
2016-08-19 18:25:38 +0900

    first comit

---
+++ Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private float speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().AddForce(
+			(transform.forward + transform.right) * speed,
+			ForceMode.VelocityChange);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Racket.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Racket : MonoBehaviour {
+
+	private float accel = 1000.0f;
+	private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*this.GetComponent<Rigidbody> ().AddForce (
+			transform.right * Input.GetAxisRaw ("Horizontal") * accel,
+			ForceMode.Impulse);*/
+		App_Touch.TouchInfo touch = App_Touch.GetTouch ();
+		if (touch == App_Touch.TouchInfo.Began) {
+
+
+		}
+		if (touch == App_Touch.TouchInfo.Moved) {
+			pos = App_Touch.GetTouchPosition();
+			pos.y = 0.0f;
+			pos.z = -6.0f;
+			transform.position = pos;
+		}
+		if (touch == App_Touch.TouchInfo.Ended) {
+		}
+	}
+
+}
